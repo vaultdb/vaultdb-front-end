7303ba1bc123140f43aa02b9d67231992b39f8b4
Jennie Rogers <jennie@northwestern.edu>
2024-12-18 16:26:24 -0600

    code cleanup

--- src/main/java/org/vaultdb/util/Utilities.java
+++ src/main/java/org/vaultdb/util/Utilities.java
@@ -67,7 +67,6 @@ public class Utilities {
     try {
       mpcLib = SystemConfiguration.getInstance().getProperty("mpc-lib");
     } catch (Exception e) {
-      // TODO Auto-generated catch block
       e.printStackTrace();
     }
     return getVaultDBRoot() + "/conf/smc/operators/" + mpcLib;
