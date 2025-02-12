47e3ffa0ffe8d164662446c33d2f1a6b04e0c529
Donghyun Sohn <44919498+donghyun-sohn@users.noreply.github.com>
2025-02-12 11:54:02 -0600

    Update q02.sql

--- src/test/java/org/vaultdb/test/sql/tpch/q02.sql
+++ src/test/java/org/vaultdb/test/sql/tpch/q02.sql
@@ -1,46 +1,23 @@
-WITH min_ps_supplycost AS (
-    SELECT  ps.ps_partkey,min(ps.ps_supplycost) min_cost
-    FROM
-      partsupp ps,
-      supplier s,
-      nation n,
-      region r
-    WHERE
-      s.s_suppkey = ps.ps_suppkey
-      and s.s_nationkey = n.n_nationkey
-      and n.n_regionkey = r.r_regionkey
-      and r.r_name = 'EUROPE'
-    GROUP BY ps.ps_partkey
-  )
-select
-  s.s_acctbal,
-  s.s_name,
-  n.n_name,
-  p.p_partkey,
-  p.p_mfgr,
-  s.s_address,
-  s.s_phone,
-  s.s_comment
-from
-  part p,
-  supplier s,
-  partsupp ps,
-  nation n,
-  region r,
-  min_ps_supplycost mps
-where
-  p.p_partkey = ps.ps_partkey
-  and s.s_suppkey = ps.ps_suppkey
-  and p.p_size = 41
-  and p.p_type like '%NICKEL'
-  and s.s_nationkey = n.n_nationkey
-  and n.n_regionkey = r.r_regionkey
-  and r.r_name = 'EUROPE'
-  and mps.ps_partkey = ps.ps_partkey
-  and ps.ps_supplycost = mps.min_cost
-order by
-  s.s_acctbal desc,
-  n.n_name,
-  s.s_name,
-  p.p_partkey
-limit 100
+SELECT s_acctbal, s_name, n_name, p_partkey, p_mfgr, s_address, s_phone, s_comment
+FROM part, supplier, partsupp, nation, region
+WHERE
+        p_partkey = ps_partkey AND s_suppkey = ps_suppkey
+  AND p_size = 41 AND p_type LIKE '%NICKEL' AND s_nationkey = n_nationkey
+  AND n_regionkey = r_regionkey AND r_name = 'EUROPE' AND ps_supplycost =
+                                                          (
+                                                              SELECT
+                                                                  MIN(ps_supplycost)
+                                                              FROM part, partsupp, supplier, nation, region
+                                                              WHERE
+                                                                      p_partkey = ps_partkey
+                                                                AND s_suppkey = ps_suppkey
+                                                                AND s_nationkey = n_nationkey
+                                                                AND n_regionkey = r_regionkey
+                                                                AND r_name = 'EUROPE'
+                                                          )
+ORDER BY
+    s_acctbal DESC,
+    n_name,
+    s_name,
+    p_partkey
+    LIMIT 100;
